ba4c2e9f6437ff2e2c120a37b95eef4edf83f46c
MFairuziIszamAziz <73611216+MFairuziIszamAziz@users.noreply.github.com>
2020-12-21 11:56:58 +0700

    Update tb_bpl.sql

--- tb_bpl.sql
+++ tb_bpl.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 17, 2020 at 01:00 PM
--- Server version: 10.4.14-MariaDB
--- PHP Version: 7.2.34
+-- Waktu pembuatan: 21 Des 2020 pada 05.57
+-- Versi server: 10.4.14-MariaDB
+-- Versi PHP: 7.2.34
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,47 +24,122 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `barang`
+-- Struktur dari tabel `barang`
 --
 
 CREATE TABLE `barang` (
   `sku` varchar(20) NOT NULL,
   `nama_brg` varchar(35) NOT NULL,
-  `depart` varchar(50) NOT NULL,
+  `id_kate` varchar(15) NOT NULL,
   `h_beli` int(255) NOT NULL,
   `h_jual` int(255) NOT NULL,
   `stok` int(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `barang`
+-- Dumping data untuk tabel `barang`
 --
 
-INSERT INTO `barang` (`sku`, `nama_brg`, `depart`, `h_beli`, `h_jual`, `stok`) VALUES
-('1', 'Rapika', 'House Hold', 30000, 20000, 17),
-('2', 'Masker', 'Non Food', 20000, 10000, 12),
-('4', 'House Hold', 'null', 30000, 40000, 22),
-('5', 'Daia', 'House Hold', 50000, 60000, 22);
+INSERT INTO `barang` (`sku`, `nama_brg`, `id_kate`, `h_beli`, `h_jual`, `stok`) VALUES
+('MI-01', 'Mie Indomie Kari Ayam', '1', 1000, 2500, 10),
+('MS-01', 'Masker Garnier', '2', 10000, 15000, 15),
+('RP-01', 'Rapika', '3', 10000, 15000, 12);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `penjualan`
+-- Struktur dari tabel `kategori`
 --
 
-CREATE TABLE `penjualan` (
-  `no_penj` varchar(20) NOT NULL,
-  `tgl` varchar(30) NOT NULL,
-  `nama_brg` varchar(35) NOT NULL,
-  `harga` int(255) NOT NULL,
-  `qty` int(255) NOT NULL,
-  `total` int(255) NOT NULL
+CREATE TABLE `kategori` (
+  `id_kate` varchar(15) NOT NULL,
+  `kategori` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data untuk tabel `kategori`
+--
+
+INSERT INTO `kategori` (`id_kate`, `kategori`) VALUES
+('1', 'Food'),
+('2', 'Non Food'),
+('3', 'House Hold');
+
 -- --------------------------------------------------------
 
 --
--- Table structure for table `user`
+-- Struktur dari tabel `laba`
+--
+
+CREATE TABLE `laba` (
+  `id` int(11) NOT NULL,
+  `noresi` varchar(50) CHARACTER SET latin1 NOT NULL,
+  `untung` int(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `laba`
+--
+
+INSERT INTO `laba` (`id`, `noresi`, `untung`) VALUES
+(1, '004', 3000),
+(2, '005', 3000),
+(3, '006', 6000);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `transaksi`
+--
+
+CREATE TABLE `transaksi` (
+  `noresi` varchar(50) NOT NULL,
+  `tanggal` varchar(20) NOT NULL,
+  `username` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `transaksi`
+--
+
+INSERT INTO `transaksi` (`noresi`, `tanggal`, `username`) VALUES
+('001', '21-12-2020', 'admin'),
+('002', '21-12-2020', 'admin'),
+('003', '21-12-2020', 'admin'),
+('004', '21-12-2020', 'admin'),
+('005', '21-12-2020', 'admin'),
+('006', '21-12-2020', 'admin');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `transaksi_detail`
+--
+
+CREATE TABLE `transaksi_detail` (
+  `id` int(11) NOT NULL,
+  `sku` varchar(35) CHARACTER SET utf8mb4 NOT NULL,
+  `noresi` varchar(50) CHARACTER SET utf8mb4 NOT NULL,
+  `jumlah` int(11) NOT NULL,
+  `harga` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `transaksi_detail`
+--
+
+INSERT INTO `transaksi_detail` (`id`, `sku`, `noresi`, `jumlah`, `harga`) VALUES
+(13, 'MI-01', '001', 2, 5000),
+(14, 'MS-01', '002', 3, 45000),
+(15, 'RP-01', '003', 4, 60000),
+(16, 'MI-01', '004', 2, 5000),
+(17, 'MI-01', '005', 2, 5000),
+(18, 'MI-01', '006', 4, 10000);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `user`
 --
 
 CREATE TABLE `user` (
@@ -75,12 +150,13 @@ CREATE TABLE `user` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `user`
+-- Dumping data untuk tabel `user`
 --
 
 INSERT INTO `user` (`Username`, `Email`, `Password`, `login_terakhir`) VALUES
-('admin', 'admin@gmail.com', 'admin', '2020-12-17'),
-('qwe', 'qwe@', 'qwe', '2020-12-17'),
+('admin', 'admin@gmail.com', 'admin', '2020-12-21'),
+('qwe', 'qwe@', 'qwe', '2020-12-20'),
+('sisfo', 'sisfo@gmail.com', 'mnmrk', '2020-12-19'),
 ('user', 'mhadefpetriza@gmail.com', 'user', '2020-12-17');
 
 --
@@ -88,16 +164,75 @@ INSERT INTO `user` (`Username`, `Email`, `Password`, `login_terakhir`) VALUES
 --
 
 --
--- Indexes for table `barang`
+-- Indeks untuk tabel `barang`
 --
 ALTER TABLE `barang`
-  ADD PRIMARY KEY (`sku`);
+  ADD PRIMARY KEY (`sku`),
+  ADD KEY `id_kate` (`id_kate`);
 
 --
--- Indexes for table `user`
+-- Indeks untuk tabel `kategori`
+--
+ALTER TABLE `kategori`
+  ADD PRIMARY KEY (`id_kate`);
+
+--
+-- Indeks untuk tabel `laba`
+--
+ALTER TABLE `laba`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `noresi` (`noresi`);
+
+--
+-- Indeks untuk tabel `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD PRIMARY KEY (`noresi`);
+
+--
+-- Indeks untuk tabel `transaksi_detail`
+--
+ALTER TABLE `transaksi_detail`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `sku` (`sku`);
+
+--
+-- Indeks untuk tabel `user`
 --
 ALTER TABLE `user`
   ADD PRIMARY KEY (`Username`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `laba`
+--
+ALTER TABLE `laba`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT untuk tabel `transaksi_detail`
+--
+ALTER TABLE `transaksi_detail`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
+
+--
+-- Ketidakleluasaan untuk tabel `barang`
+--
+ALTER TABLE `barang`
+  ADD CONSTRAINT `barang_ibfk_1` FOREIGN KEY (`id_kate`) REFERENCES `kategori` (`id_kate`);
+
+--
+-- Ketidakleluasaan untuk tabel `transaksi_detail`
+--
+ALTER TABLE `transaksi_detail`
+  ADD CONSTRAINT `transaksi_detail_ibfk_1` FOREIGN KEY (`sku`) REFERENCES `barang` (`sku`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
